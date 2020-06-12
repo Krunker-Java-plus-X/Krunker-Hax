9b57b87e304af2cf499a063b12b4480f9c674ab8
Krunker-Java-plus-X <66849899+Krunker-Java-plus-X@users.noreply.github.com>
2020-06-13 00:25:47 +0200

    Create Krunker ESP.js

---
+++ Krunker ESP.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Krunker NameTags
+// @namespace    GOD CHEAT
+// @version      2.5.1
+// @description  Why am I able to see names through walls
+// @author       JA plus X
+// @match        *://krunker.io/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let initialize = function(data) {
+        let regex = /if\(!\w+\['(\w+)']\)continue/; //Hook
+        let result = regex.exec(data);
+        if (result) {
+            const inView = result[1];
+            const push = Array.prototype.push;
+            Array.prototype.push = function(...args) {
+                push.apply(this, args);
+                if (args[0] instanceof Object && args[0].isPlayer) { //isPlayer is the value. If this get patched change isPlayer to new value to make it work
+                    Object.defineProperty(args[0], inView, {value: true, configurable: false});
+                }
+            }
+        }
+    }
+    //Hook Control
+    const decode = window.TextDecoder.prototype.decode;
+    window.TextDecoder.prototype.decode = function(...args) {
+        let data = decode.apply(this, args);
+        if (data.length > 1050000) {
+            initialize(data);
+        }
+        return data;
+    }
+})();
